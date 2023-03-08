ba705f385dda63056fe483535c56f2cb176fa158
Federico Merino <fedemerino00@gmail.com>
2023-03-07 21:39:41 -0300

    responsive

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,50 +1,61 @@
-import React from "react";
-import StyledButton from "./StyledButton";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 const Nav = () => {
+  const [buttonState, setButtonState] = useState(false);
+
+  const handleButton = () => {
+    setButtonState(!buttonState);
+  };
   return (
-    <div className="nav">
-      <Link to="/" style={{ textDecoration: "none" }}>
-        <div className="navLinkContainer">
-          <div className="navLink">
-          <div style={{display: 'flex'}}>
-              <p className="navLinkNumber">01.</p>
-              <p className="navLinkText">HOME</p>
+    <>
+    <div className="sandwichButton" onClick={handleButton}>
+        <img src="hamburger.png" alt="" />
+      </div>
+      <div className="nav" style={{display: buttonState && 'flex'}}>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="navLinkContainer">
+            <div className="navLink">
+              <div style={{ display: "flex" }}>
+                <p className="navLinkNumber">01.</p>
+                <p className="navLinkText">HOME</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/about" style={{ textDecoration: "none" }}>
-        <div className="navLinkContainer">
-          <div className="navLink">
-            <div style={{display: 'flex'}}>
-              <p className="navLinkNumber">02.</p>
-              <p className="navLinkText">ABOUT ME</p>
+        </Link>
+        <Link to="/about" style={{ textDecoration: "none" }}>
+          <div className="navLinkContainer">
+            <div className="navLink">
+              <div style={{ display: "flex" }}>
+                <p className="navLinkNumber">02.</p>
+                <p className="navLinkText">ABOUT ME</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/projects" style={{ textDecoration: "none" }}>
-        <div className="navLinkContainer">
-          <div className="navLink">
-          <div style={{display: 'flex'}}>
-              <p className="navLinkNumber">03.</p>
-              <p className="navLinkText">PROJECTS</p>
+        </Link>
+        <Link to="/projects" style={{ textDecoration: "none" }}>
+          <div className="navLinkContainer">
+            <div className="navLink">
+              <div style={{ display: "flex" }}>
+                <p className="navLinkNumber">03.</p>
+                <p className="navLinkText">PROJECTS</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-      <Link to="/contact" style={{ textDecoration: "none" }}>
-        <div className="navLinkContainer">
-          <div className="navLink">
-          <div style={{display: 'flex'}}>
-              <p className="navLinkNumber">04.</p>
-              <p className="navLinkText">CONTACT</p>
+        </Link>
+        <Link to="/contact" style={{ textDecoration: "none" }}>
+          <div className="navLinkContainer">
+            <div className="navLink">
+              <div style={{ display: "flex" }}>
+                <p className="navLinkNumber">04.</p>
+                <p className="navLinkText">CONTACT</p>
+              </div>
             </div>
           </div>
-        </div>
-      </Link>
-    </div>
+        </Link>
+      </div>
+      
+    </>
   );
 };
 
