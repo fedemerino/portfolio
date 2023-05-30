c444b440c6b3aaf7aefc4ae4adc5893a902de844
Federico Merino <107087480+fedemerino@users.noreply.github.com>
2023-05-30 19:34:27 -0300

    Update AboutMe.jsx

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -15,7 +15,7 @@ const AboutMe = () => {
                         Back then I managed to create a calculator and a web browser in Visual Basic.
                         It wasn't until I got into University that I started coding again.
                         I learnt Algorithms and Data Structures in Python and Pascal, besides C and OOP concepts.
-                        Nowadays I'm a Frontend Developer and I'm also studying Systems Engineering at University and Backend Development.
+                        I'm currently working as a Fullstack Developer at Artfos and I'm also studying Systems Engineering at University.
                         In my free time I enjoy listening to music, playing the guitar, going for a run and learning new technologies.
                     </div>
                     <div className="tech whoiamDescription">
@@ -27,10 +27,17 @@ const AboutMe = () => {
                             <li><span className='liDot'>- </span>TypeScript</li>
                             <li><span className='liDot'>- </span>ReactJs</li>
                             <li><span className='liDot'>- </span>NextJs</li>
+                            <li><span className='liDot'>- </span>Redux</li>
+                            <li><span className='liDot'>- </span>Vue</li>
+                            <li><span className='liDot'>- </span>PHP</li>
+                            <li><span className='liDot'>- </span>Node</li>
+                            <li><span className='liDot'>- </span>Express</li>
+                            <li><span className='liDot'>- </span>MongoDB</li>
+                            <li><span className='liDot'>- </span>Firebase</li>
                             <li><span className='liDot'>- </span>SASS</li>
                             <li><span className='liDot'>- </span>Bootstrap</li>
-                            <li><span className='liDot'>- </span>MaterialUI</li>
-                            <li><span className='liDot'>- </span>Firebase</li>
+                            <li><span className='liDot'>- </span>Tailwind</li>
+                            <li><span className='liDot'>- </span>MaterialUI</li>                
                             <li><span className='liDot'>- </span>Git</li>
                             <li><span className='liDot'>- </span>GitHub</li>
                         </ul>
